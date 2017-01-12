5393c4915e13942880e9b84fa25f41ba55b58019
chenxs <hinamikux@gmail.com>
2017-01-12 19:38:01 +0800

    codemirror, showdown

--- js/index.js
+++ js/index.js
@@ -1,14 +1,26 @@
 $(function () {
 
+    var converter = new showdown.Converter();
+
     var editor = new Vue({
         el: '#editor',
         data: {
             markdown_text: ''
         },
         computed: {
-            preview_text: function() {
-                return markdown.toHTML(this.markdown_text);
+            preview_text: function () {
+                return converter.makeHtml(this.markdown_text);
             }
         }
+    });
+
+    var markdown = CodeMirror.fromTextArea($('#markdown_editor')[0], {
+        lineNumbers: true,
+        theme: 'the-matrix',
+        mode: 'markdown'
+    });
+
+    markdown.on('changes', function(instance) {
+        editor.markdown_text = instance.getValue();
     });
 });
